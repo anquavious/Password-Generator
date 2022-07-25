8940a7f17c1b75623c9fb25ca5cf1035504499f5
anquavious <agrant022@gmail.com>
2022-07-25 14:38:52 -0400

    Password-Generator

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # Password-Generator
+
+# Description
+
+This application was created to generate a random password for employees based on criteria that they have selected.The password includes special characters, numbers, lowercase, and uppercase letters. It will assist employees in having a secure and strong password that no one else can guess, which enables extra safety.
+
+# Usage
+
+The user will select the red button titled "Generate Password". Once the user selects this button a prompt will appear asking the user how many characters do they want the password to have. Once the user answers this question, a message will repeat back how many characters the password will have. The user will then be presented with questions to confirm if they would like uppercase, lowercase, numbers, and special characters. A password will then appear with a mixture of these characters within the text area. The password cannot contain less than 8 characters and more than 128 characters. 
+
+![](Screenshot%20.jpeg)
+![](Screenshot2.jpeg)
+# License
+
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+Copyright © 2022 <Anquavious Grant>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
--- script.js
+++ script.js
@@ -1,15 +1,83 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
+// 1. Prompt the user for the password criteria
+// Password lenth 8 < 128
+// lowercase, uppercase, special characters
+// 2.Validate the input
+// 3. Generate password based on criteria
+// 4. Display password on the page
 
+//Assignment Code
+document.querySelector("#generate").addEventListener("click", writePassword);
+
+// Arrays for letters, numbers, and special characters
+const number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+const specialCharacter = [":", ";", "@", "!", "%", "&", ",", "*", "$", "<", "/", ">", "+", "-"];
+const uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+const lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+// Variables to confirm for password
+var confirmLowerCase;
+var passwordLength = 0;
+var confirmSpecialCharacter;
+var confirmNumber;
+var confirmUpperCase;
+
+//function to generate the password button
+function generatePassword() {
+var confirmLength = prompt("How many characters would you like your password to have?");
+
+  // Loop if answer is outside the parameters 
+  if(confirmLength < 8 || confirmLength > 128) {
+      alert("Password length is incorrect Please try again!");
+      return null
+      } 
+
+      // Repeat back how many charactes the user will have  
+      alert(`Your password will have ${confirmLength} characters`);
+
+    // Determine parameters of password 
+    var confirmSpecialCharacter = confirm("Select OK to confirm if you would like to include special characters");
+    var confirmNumber = confirm("Select OK to confirm if you would like to include numeric characters");    
+    var confirmLowerCase = confirm("Select OK to confirm if you would like to include lowercase characters");
+    var confirmUpperCase = confirm("Select OK to confirm if you would like to include uppercase characters");
+      // Loop incase user statement is not within parameters
+      if(confirmUpperCase === false && confirmLowerCase === false && confirmSpecialCharacter === false && confirmNumericCharacter === false) {
+        alert("You must choose at least one parameter");
+      }
+
+      // Assign an action to the password
+      var passwordCharacters = []
+      
+    if (confirmSpecialCharacter && confirmLowerCase && confirmUpperCase) {
+      passwordCharacters = passwordCharacters.concat(specialCharacter, confirmUpperCase, confirmLowerCase)
+      
+    }
+
+    if (confirmNumber) {
+      passwordCharacters = passwordCharacters.concat(number)
+    
+    }
+      
+    if (confirmLowerCase) {
+      passwordCharacters = passwordCharacters.concat(lowercase)
+    }
+    
+    if (confirmUpperCase) {
+      passwordCharacters = passwordCharacters.concat(uppercase)
+      console.log(passwordCharacters)
+    }
+
+      //Loop selecting random characters from the array
+      var random = "";
+      
+      for (let p = 0; p < confirmLength; p++) {
+        random = random + passwordCharacters[Math.floor(Math.random() * passwordCharacters.length)];
+      }
+      return random;
+}
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
